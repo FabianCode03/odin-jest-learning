caf0fa3b6e5ab79eb70bd57f9cd9e48b35289270
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-08 03:01:21 +0200

    Improve capitalize.test with types and DRY

--- src/modules/capitalize.test.ts
+++ src/modules/capitalize.test.ts
@@ -1,18 +1,43 @@
 import { capitalize } from './capitalize';
+import TestCase from './TestCase_interface';
 
-test('capitalized string stays the same', () => {
-  const input = 'Fabian';
-  const output = 'Fabian';
-  expect(capitalize(input)).toEqual(output);
-});
+const testCases: TestCase[] = [
+  {
+    description: 'capitalized string stays the same',
+    input: 'Fabian',
+    expectedOutput: 'Fabian',
+  },
+  {
+    description: 'empty string is unchanged',
+    input: '',
+    expectedOutput: '',
+  },
+  {
+    description: 'Works with special characters',
+    input: '!',
+    expectedOutput: '!',
+  },
+  {
+    description: 'lowercase strings are capitalized (1)',
+    input: 'fabian',
+    expectedOutput: 'Fabian',
+  },
+  {
+    description: 'lowercase strings are capitalized (2)',
+    input: 'helen',
+    expectedOutput: 'Helen',
+  },
+  {
+    description: 'lowercase strings are capitalized (3)',
+    input: 'fiona',
+    expectedOutput: 'Fiona',
+  },
+];
 
-test('lowercase strings are capitalized', () => {
-  const data = [
-    { input: 'fabian', output: 'Fabian' },
-    { input: 'helen', output: 'Helen' },
-    { input: 'fiona', output: 'Fiona' },
-  ];
-  for (const str of data) {
-    expect(capitalize(str.input)).toBe(str.output);
-  }
+describe('function that capitalizes first character of the String', () => {
+  testCases.forEach(({ description, input, expectedOutput }) => {
+    test(description, () => {
+      expect(capitalize(input)).toBe(expectedOutput);
+    });
+  });
 });
